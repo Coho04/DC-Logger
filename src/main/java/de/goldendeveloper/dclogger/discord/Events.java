22d707a6ff0e616597c55571a4cf9fe115648c67
Collin Ilgner <c.ilgner@tmg-web.de>
2022-03-18 15:39:28 +0100

    ADD: Join Leave Channel Event

--- src/main/java/de/goldendeveloper/dclogger/discord/Events.java
+++ src/main/java/de/goldendeveloper/dclogger/discord/Events.java
@@ -16,6 +16,9 @@ import net.dv8tion.jda.api.events.guild.member.update.GuildMemberUpdateNicknameE
 import net.dv8tion.jda.api.events.guild.update.GuildUpdateAfkChannelEvent;
 import net.dv8tion.jda.api.events.guild.update.GuildUpdateBannerEvent;
 import net.dv8tion.jda.api.events.guild.update.GuildUpdateMaxMembersEvent;
+import net.dv8tion.jda.api.events.guild.voice.GuildVoiceJoinEvent;
+import net.dv8tion.jda.api.events.guild.voice.GuildVoiceLeaveEvent;
+import net.dv8tion.jda.api.events.guild.voice.GuildVoiceMoveEvent;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
 import net.dv8tion.jda.api.events.message.react.MessageReactionAddEvent;
 import net.dv8tion.jda.api.events.message.react.MessageReactionRemoveEvent;
@@ -45,58 +48,71 @@ public class Events extends ListenerAdapter {
     @Override
     public void onSlashCommandInteraction(SlashCommandInteractionEvent e) {
         if (e.isFromGuild()) {
-            onEvent(e.getGuild(), "Command", "", e.getTextChannel());
+            onEvent(e.getGuild(), "Command", "Der User " + e.getUser().getName() + " hat den Command " + e.getName() + e.getSubcommandName(), e.getTextChannel());
         }
     }
 
     @Override
     public void onGuildMemberRemove(GuildMemberRemoveEvent e) {
-        onEvent(e.getGuild(), "", "", null);
+        onEvent(e.getGuild(), "Server Verlassen", "Der User " + e.getUser().getName() + " hat den Server verlassen!", null);
     }
 
     @Override
     public void onGuildMemberJoin(GuildMemberJoinEvent e) {
-        onEvent(e.getGuild(), "", "", null);
+        onEvent(e.getGuild(), "Server join", "Der User " + e.getUser().getName() + " ist dem Server beigetreten!", null);
     }
 
     @Override
     public void onGuildMemberRoleAdd(GuildMemberRoleAddEvent e) {
-        onEvent(e.getGuild(), "", "", null);
+        onEvent(e.getGuild(), "Role hinzugefügt", "Dem User " + e.getUser().getName() + " wurde die Rolle <role> gegeben!", null);
     }
 
     @Override
     public void onGuildMemberRoleRemove(GuildMemberRoleRemoveEvent e) {
-        onEvent(e.getGuild(), "", "", null);
+        onEvent(e.getGuild(), "Rolle entfernt", "Dem User " + e.getUser().getName() + " wurde die Rolle <role> entfernt!", null);
     }
 
     @Override
     public void onGuildUpdateAfkChannel(GuildUpdateAfkChannelEvent e) {
-        onEvent(e.getGuild(), "", "", null);
+        onEvent(e.getGuild(), "AFK Channel Update", "Der Neue AFK Channel ist nun " + e.getNewAfkChannel()  + " Alte: " + e.getOldAfkChannel(), null);
     }
 
     @Override
     public void onGuildUpdateBanner(GuildUpdateBannerEvent e) {
-        onEvent(e.getGuild(), "", "", null);
+        onEvent(e.getGuild(), "Banner Update", "Der Alte Banner wurde auf einen Neuen  geändert!", null);
     }
 
     @Override
     public void onGuildMemberUpdateNickname(GuildMemberUpdateNicknameEvent e) {
-        onEvent(e.getGuild(), "", "", null);
+        onEvent(e.getGuild(), "Nickname Update", "Der User " + e.getOldNickname() + " heißt nun " +  e.getNewNickname() + "!", null);
     }
 
     @Override
     public void onGuildMemberUpdateBoostTime(GuildMemberUpdateBoostTimeEvent e) {
-        onEvent(e.getGuild(), "", "", null);
+        onEvent(e.getGuild(), "Time Boosted Update", "" + e.getOldTimeBoosted() + " Neu " + e.getNewTimeBoosted(), null);
     }
 
     @Override
     public void onGuildUpdateMaxMembers(GuildUpdateMaxMembersEvent e) {
-        onEvent(e.getGuild(), "", "", null);
+        onEvent(e.getGuild(), "Update Maximale User", "Die Anzahl von Maximalen Usern hat sich geändert! Alt: " + e.getOldMaxMembers() + " Neu: " + e.getNewMaxMembers() + "!", null);
     }
 
     @Override
     public void onGuildUnban(GuildUnbanEvent e) {
+        onEvent(e.getGuild(), "Server Unban User", "Der User " + e.getUser().getName() + " hat oder wurde entbannt!", null);
+    }
 
+    @Override
+    public void onGuildVoiceJoin(GuildVoiceJoinEvent e) {
+        onEvent(e.getGuild(), "User Channel Join", "Der User " + e.getMember().getUser().getName() + " ist dem Channel " + e.getChannelJoined().getAsMention() + " gejoined!", null);
+    }
+    @Override
+    public void onGuildVoiceLeave(GuildVoiceLeaveEvent e) {
+        onEvent(e.getGuild(), "User Channel Leave", "Der User " + e.getMember().getUser().getName() + " hat den Channel " + e.getChannelLeft().getAsMention() + " verlassen!", null);
+    }
+    @Override
+    public void onGuildVoiceMove(GuildVoiceMoveEvent e) {
+        onEvent(e.getGuild(), "User Channel Move", "Der User " + e.getMember().getUser().getName() + " hat den Channel " + e.getChannelLeft().getAsMention() + " verlassen und ist dem Channel " + e.getChannelJoined() + " beigetreten!", null);
     }
 
     private MessageEmbed onEmbed(Guild guild, String Event, String Value) {
