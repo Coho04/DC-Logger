eb1d79574c7b56db21cca7c8675ee974e207a5e4
c.ilgner <cohohohn04@gmail.com>
2023-08-10 21:05:20 +0200

    FIX: Command Registration

--- src/main/java/de/goldendeveloper/logger/Main.java
+++ src/main/java/de/goldendeveloper/logger/Main.java
@@ -1,11 +1,9 @@
 package de.goldendeveloper.logger;
 
 import de.goldendeveloper.dcbcore.DCBotBuilder;
-import de.goldendeveloper.dcbcore.interfaces.CommandInterface;
 import de.goldendeveloper.logger.discord.CustomEvents;
 import de.goldendeveloper.logger.discord.commands.Settings;
 
-import java.util.LinkedList;
 
 public class Main {
 
@@ -15,15 +13,9 @@ public class Main {
         CustomConfig config = new CustomConfig();
         DCBotBuilder dcBotBuilder = new DCBotBuilder(args, true);
         dcBotBuilder.registerEvents(new CustomEvents());
-        dcBotBuilder.registerCommands(registerCommands());
+        dcBotBuilder.registerCommands(new Settings());
         dcBotBuilder.build();
         mysqlConnection = new MysqlConnection(config.getMysqlHostname(), config.getMysqlUsername(), config.getMysqlPassword(), config.getMysqlPort());
-    }
-
-    public static LinkedList<CommandInterface> registerCommands() {
-        LinkedList<CommandInterface> commands = new LinkedList<>();
-        commands.add(new Settings());
-        return commands;
     }
 
     public static MysqlConnection getMysqlConnection() {
